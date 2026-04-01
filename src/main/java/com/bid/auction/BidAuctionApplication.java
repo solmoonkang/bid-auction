42c3c9a5c66270b6fb4abf99c2ec19b77148b398
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2026-04-01 17:22:08 +0900

    초기 세팅

---
+++ src/main/java/com/bid/auction/BidAuctionApplication.java
@@ -0,0 +1,13 @@
+package com.bid.auction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidAuctionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BidAuctionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bid/auction/BidAuctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bid.auction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BidAuctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
