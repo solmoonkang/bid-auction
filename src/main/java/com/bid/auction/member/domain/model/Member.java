68879806ec6e0400a5e619f38b9230862f06a0b7
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2026-04-02 02:48:54 +0900

    feat(#1): 회원 도메인 모델 구현

---
+++ src/main/java/com/bid/auction/member/domain/model/Member.java
@@ -0,0 +1,67 @@
+package com.bid.auction.member.domain.model;
+
+import org.hibernate.annotations.Comment;
+
+import com.bid.auction.global.common.BaseMappingEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(
+	name = "MEMBERS",
+	uniqueConstraints = {
+		@UniqueConstraint(name = "uk_member_email", columnNames = "email"),
+		@UniqueConstraint(name = "uk_member_nickname", columnNames = "nickname"),
+		@UniqueConstraint(name = "uk_member_phone_number", columnNames = "phone_number")
+	}
+)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member extends BaseMappingEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "member_id")
+	private Long id;
+
+	@Comment("이메일")
+	@Column(name = "email", nullable = false, length = 100)
+	private String email;
+
+	@Comment("비밀번호")
+	@Column(name = "password", nullable = false)
+	private String password;
+
+	@Comment("이름")
+	@Column(name = "name", nullable = false, length = 50)
+	private String name;
+
+	@Comment("닉네임")
+	@Column(name = "nickname", nullable = false, length = 50)
+	private String nickname;
+
+	@Comment("전화번호")
+	@Column(name = "phone_number", nullable = false, length = 20)
+	private String phoneNumber;
+
+	private Member(String email, String password, String name, String nickname, String phoneNumber) {
+		this.email = email;
+		this.password = password;
+		this.name = name;
+		this.nickname = nickname;
+		this.phoneNumber = phoneNumber;
+	}
+
+	public static Member signUp(String email, String password, String name, String nickname, String phoneNumber) {
+		return new Member(email, password, name, nickname, phoneNumber);
+	}
+}
