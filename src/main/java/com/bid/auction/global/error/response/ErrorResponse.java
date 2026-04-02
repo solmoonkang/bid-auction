ac1fea03b8d84d68434b8a9eb6072073408362da
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2026-04-02 19:02:17 +0900

    feat(#1): 공통 예외 응답 DTO 정의

---
+++ src/main/java/com/bid/auction/global/error/response/ErrorResponse.java
@@ -0,0 +1,27 @@
+package com.bid.auction.global.error.response;
+
+import java.util.Map;
+
+import com.bid.auction.global.error.model.ErrorCode;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.Builder;
+
+@Builder
+public record ErrorResponse(
+	String code,
+
+	String message,
+
+	@JsonInclude(JsonInclude.Include.NON_EMPTY)
+	Map<String, String> validation
+) {
+
+	public static ErrorResponse of(ErrorCode errorCode) {
+		return new ErrorResponse(errorCode.getCode(), errorCode.getMessage(), null);
+	}
+
+	public static ErrorResponse of(ErrorCode errorCode, Map<String, String> validation) {
+		return new ErrorResponse(errorCode.getCode(), errorCode.getMessage(), validation);
+	}
+}
