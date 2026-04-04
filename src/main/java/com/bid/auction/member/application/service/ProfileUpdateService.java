f59691e5ff788f12cb373f07ee7847e55ab2d758
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2026-04-04 13:34:43 +0900

    feat(#1): 회원 프로필 정보 업데이트에 따른 캐시 초기화 로직 추가

--- src/main/java/com/bid/auction/member/application/service/ProfileUpdateService.java
+++ src/main/java/com/bid/auction/member/application/service/ProfileUpdateService.java
@@ -3,6 +3,7 @@ package com.bid.auction.member.application.service;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.bid.auction.member.application.component.MemberCacheProcessor;
 import com.bid.auction.member.application.component.MemberFinder;
 import com.bid.auction.member.application.dto.request.EmailUpdateRequest;
 import com.bid.auction.member.application.dto.request.NicknameUpdateRequest;
@@ -20,15 +21,20 @@ public class ProfileUpdateService {
 	private final MemberFinder memberFinder;
 
 	private final MemberValidator memberValidator;
+	private final MemberCacheProcessor memberCacheProcessor;
 
 	public void updateEmail(Long memberId, EmailUpdateRequest emailUpdateRequest) {
 		final Member member = memberFinder.findById(memberId);
+		final String previousEmail = member.getEmail();
 
 		if (member.isSameEmail(emailUpdateRequest.email()))
 			return;
 
 		memberValidator.validateEmailUniqueness(emailUpdateRequest.email());
 		member.updateEmail(emailUpdateRequest.email());
+
+		memberCacheProcessor.evictMemberCache(memberId, previousEmail);
+		memberCacheProcessor.evictMemberCache(memberId, member.getEmail());
 	}
 
 	public void updateNickname(Long memberId, NicknameUpdateRequest nicknameUpdateRequest) {
@@ -39,6 +45,8 @@ public class ProfileUpdateService {
 
 		memberValidator.validateNicknameUniqueness(nicknameUpdateRequest.nickname());
 		member.updateNickname(nicknameUpdateRequest.nickname());
+
+		memberCacheProcessor.evictMemberCache(memberId, member.getEmail());
 	}
 
 	public void updatePhoneNumber(Long memberId, PhoneNumberUpdateRequest phoneNumberUpdateRequest) {
@@ -49,5 +57,7 @@ public class ProfileUpdateService {
 
 		memberValidator.validatePhoneNumberUniqueness(phoneNumberUpdateRequest.phoneNumber());
 		member.updatePhoneNumber(phoneNumberUpdateRequest.phoneNumber());
+
+		memberCacheProcessor.evictMemberCache(memberId, member.getEmail());
 	}
 }
