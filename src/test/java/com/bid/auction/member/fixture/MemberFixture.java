070c9143037ebef43cfb0da44838e128f6a5078d
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2026-04-08 20:53:58 +0900

    test(#1): 회원가입 요청 DTO 핼퍼 로직 구현

---
+++ src/test/java/com/bid/auction/member/fixture/MemberFixture.java
@@ -0,0 +1,61 @@
+package com.bid.auction.member.fixture;
+
+import com.bid.auction.member.application.dto.request.SignUpRequest;
+
+public class MemberFixture {
+
+	public static SignUpRequest aSignUpRequest() {
+		return new SignUpRequest(
+			"test@example.com",
+			"password123!",
+			"password123!",
+			"testName",
+			"testNickname",
+			"01023456789"
+		);
+	}
+
+	public static SignUpRequest aSignUpRequestWithEmail(String email) {
+		return new SignUpRequest(
+			email,
+			"password123!",
+			"password123!",
+			"testName",
+			"testNickname",
+			"01023456789"
+		);
+	}
+
+	public static SignUpRequest aSignUpRequestWithNickname(String nickname) {
+		return new SignUpRequest(
+			"test@example.com",
+			"password123!",
+			"password123!",
+			"testName",
+			nickname,
+			"01023456789"
+		);
+	}
+
+	public static SignUpRequest aSignUpRequestWithPhoneNumber(String phoneNumber) {
+		return new SignUpRequest(
+			"test@example.com",
+			"password123!",
+			"password123!",
+			"testName",
+			"testNickname",
+			phoneNumber
+		);
+	}
+
+	public static SignUpRequest aSignUpRequestWithPasswordMismatch() {
+		return new SignUpRequest(
+			"test@example.com",
+			"password123!",
+			"wrongPassword123!",
+			"testName",
+			"testNickname",
+			"01023456789"
+		);
+	}
+}
