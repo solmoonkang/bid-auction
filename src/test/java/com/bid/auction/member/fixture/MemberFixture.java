6bfcedf57bb6d4200f0eca680cc703edfca9f4b9
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2026-04-11 00:15:16 +0900

    test(#1): 회원 객체 생성 핼퍼 로직 구현

--- src/test/java/com/bid/auction/member/fixture/MemberFixture.java
+++ src/test/java/com/bid/auction/member/fixture/MemberFixture.java
@@ -1,9 +1,30 @@
 package com.bid.auction.member.fixture;
 
+import com.bid.auction.member.application.dto.request.EmailUpdateRequest;
 import com.bid.auction.member.application.dto.request.SignUpRequest;
 import com.bid.auction.member.domain.model.Member;
 
 public class MemberFixture {
+
+	public static Member aMember() {
+		return Member.signUp(
+			"test@example.com",
+			"encodedPassword123!",
+			"testName",
+			"testNickname",
+			"01023456789"
+		);
+	}
+
+	public static Member aMemberWithEmail(String email) {
+		return Member.signUp(
+			email,
+			"encodedPassword123!",
+			"testName",
+			"testNickname",
+			"01023456789"
+		);
+	}
 
 	public static SignUpRequest aSignUpRequest() {
 		return new SignUpRequest(
@@ -60,13 +81,9 @@ public class MemberFixture {
 		);
 	}
 
-	public static Member aMember() {
-		return Member.signUp(
-			"test@example.com",
-			"encodedPassword123!",
-			"testName",
-			"testNickname",
-			"01023456789"
+	public static EmailUpdateRequest aEmailUpdateRequest(String email) {
+		return new EmailUpdateRequest(
+			email
 		);
 	}
 }
