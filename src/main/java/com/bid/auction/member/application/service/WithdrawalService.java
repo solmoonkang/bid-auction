32907b1965d0592f492310e1b4e56b418cb437ec
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2026-04-13 18:36:03 +0900

    feat(#1): 회원 탈퇴 시 비밀번호 요청 로직 추가

--- src/main/java/com/bid/auction/member/application/service/WithdrawalService.java
+++ src/main/java/com/bid/auction/member/application/service/WithdrawalService.java
@@ -6,8 +6,11 @@ import java.time.LocalDateTime;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.bid.auction.global.error.model.ErrorCode;
 import com.bid.auction.member.application.component.MemberCacheProcessor;
 import com.bid.auction.member.application.component.MemberFinder;
+import com.bid.auction.member.application.dto.request.WithdrawalRequest;
+import com.bid.auction.member.application.validator.MemberValidator;
 import com.bid.auction.member.domain.model.Member;
 
 import lombok.RequiredArgsConstructor;
@@ -19,12 +22,15 @@ public class WithdrawalService {
 
 	private final MemberFinder memberFinder;
 
+	private final MemberValidator memberValidator;
 	private final MemberCacheProcessor memberCacheProcessor;
 	private final Clock clock;
 
-	public void withdraw(Long memberId) {
+	public void withdraw(Long memberId, WithdrawalRequest withdrawalRequest) {
 		final Member member = memberFinder.findById(memberId);
 		final String previousEmail = member.getEmail();
+
+		memberValidator.validatePassword(withdrawalRequest.password(), member.getPassword(), ErrorCode.WRONG_PASSWORD);
 
 		member.withdraw(LocalDateTime.now(clock));
 
