42cd364e4c2fc98e064c12f9203bf6d1e6ed588c
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2026-04-15 18:28:38 +0900

    feat(#1): 로컬 캐싱 설정 추가

---
+++ src/main/java/com/bid/auction/global/config/CacheConfig.java
@@ -0,0 +1,9 @@
+package com.bid.auction.global.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+}
